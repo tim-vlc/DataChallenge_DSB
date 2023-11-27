e68af773e89e12ef1d677492cc5bbefd931564ad
T1m0uss3 <62285023+tim-vlc@users.noreply.github.com>
2023-11-27 22:40:08 +0100

    Update of first submission

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -9,25 +9,28 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import TimeSeriesSplit, cross_val_score
 
 from pathlib import Path
+import os
 import numpy as np
-import problem
-import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-kag = 0
+api = KaggleApi()
+api.authenticate()
 
-if kag:
-    api = KaggleApi()
-    api.authenticate()
+api.competition_download_file('mdsb-2023', 'final_test.parquet')
+api.dataset_download_file('timvlc/train-data-challenge-dsb', file_name='train.parquet')
 
-    files_to_download = ['final_test.parquet']
-    for file in files_to_download:
-        api.competition_download_file('mdsb-2023', file)
+def _read_data(path, f_name):
+    _target_column_name = "log_bike_count"
 
-        if Path(file+'.zip').is_file():
-            with zipfile.ZipFile(file + '.zip', 'r') as zipref:
-                zipref.extractall()
-    X_final = pd.read_parquet(Path('final_test.parquet'))
+    data = pd.read_parquet(os.path.join(path, "data", f_name))
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+X_final = pd.read_parquet(Path('final_test.parquet'))
+X_train, y_train = _read_data(path='.', f_name='train.parquet')
 
 def _encode_dates(X):
     X = X.copy()  # modify a copy of X
@@ -61,34 +64,15 @@ def get_estimator():
 
     return pipe
 
-X_train, y_train = problem.get_train_data()
-X_test, y_test = problem.get_test_data()
-
 pipe = get_estimator()
+pipe.fit(X_train, y_train)
 
-print(
-    f"Train set, RMSE={mean_squared_error(y_train, pipe.predict(X_train), squared=False):.2f}"
-)
-print(
-    f"Test set, RMSE={mean_squared_error(y_test, pipe.predict(X_test), squared=False):.2f}"
-)
+y_pred = pipe.predict(X_final)
 
-cv = TimeSeriesSplit(n_splits=6)
-
-# When using a scorer in scikit-learn it always needs to be better when smaller, hence the minus sign.
-scores = cross_val_score(
-    pipe, X_train, y_train, cv=cv, scoring="neg_root_mean_squared_error"
-)
-print("RMSE: ", scores)
-print(f"RMSE (all folds): {-scores.mean():.3} ± {(-scores).std():.3}")
-
-if kag:
-    y_pred = pipe.predict(X_final)
-    
-    results = pd.DataFrame(
-        dict(
-            Id=np.arange(y_pred.shape[0]),
-            log_bike_count=y_pred,
-        )
+results = pd.DataFrame(
+    dict(
+        Id=np.arange(y_pred.shape[0]),
+        log_bike_count=y_pred,
     )
-    results.to_csv("submission.csv", index=False)
+)
+results.to_csv("submission.csv", index=False)

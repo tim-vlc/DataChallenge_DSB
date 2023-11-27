a12308e95f225e53651b10be1fb6af145ab64c84
T1m0uss3 <62285023+tim-vlc@users.noreply.github.com>
2023-11-27 22:23:11 +0100

    First submission test

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -5,7 +5,29 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score
 
+from pathlib import Path
+import numpy as np
+import problem
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+kag = 0
+
+if kag:
+    api = KaggleApi()
+    api.authenticate()
+
+    files_to_download = ['final_test.parquet']
+    for file in files_to_download:
+        api.competition_download_file('mdsb-2023', file)
+
+        if Path(file+'.zip').is_file():
+            with zipfile.ZipFile(file + '.zip', 'r') as zipref:
+                zipref.extractall()
+    X_final = pd.read_parquet(Path('final_test.parquet'))
 
 def _encode_dates(X):
     X = X.copy()  # modify a copy of X
@@ -38,3 +60,35 @@ def get_estimator():
     pipe = make_pipeline(date_encoder, preprocessor, regressor)
 
     return pipe
+
+X_train, y_train = problem.get_train_data()
+X_test, y_test = problem.get_test_data()
+
+pipe = get_estimator()
+
+print(
+    f"Train set, RMSE={mean_squared_error(y_train, pipe.predict(X_train), squared=False):.2f}"
+)
+print(
+    f"Test set, RMSE={mean_squared_error(y_test, pipe.predict(X_test), squared=False):.2f}"
+)
+
+cv = TimeSeriesSplit(n_splits=6)
+
+# When using a scorer in scikit-learn it always needs to be better when smaller, hence the minus sign.
+scores = cross_val_score(
+    pipe, X_train, y_train, cv=cv, scoring="neg_root_mean_squared_error"
+)
+print("RMSE: ", scores)
+print(f"RMSE (all folds): {-scores.mean():.3} ± {(-scores).std():.3}")
+
+if kag:
+    y_pred = pipe.predict(X_final)
+    
+    results = pd.DataFrame(
+        dict(
+            Id=np.arange(y_pred.shape[0]),
+            log_bike_count=y_pred,
+        )
+    )
+    results.to_csv("submission.csv", index=False)

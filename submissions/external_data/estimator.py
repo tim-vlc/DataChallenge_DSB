86509c554e0d031f1fd7239ae75e77239603f742
T1m0uss3 <62285023+tim-vlc@users.noreply.github.com>
2023-11-28 14:01:46 +0100

    External data addition

--- submissions/external_data/estimator.py
+++ submissions/external_data/estimator.py
@@ -8,7 +8,28 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score
 
+from pathlib import Path
+import os
+import numpy as np
+from xgboost import XGBRegressor
+
+
+def _read_data(path, f_name):
+    _target_column_name = "log_bike_count"
+
+    data = pd.read_parquet(os.path.join(path, "data", f_name))
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+X_final = pd.read_parquet(Path('../../data/final_test.parquet'))
+X_train, y_train = _read_data(path='../../', f_name='train.parquet')
+X_test, y_test = _read_data(path='../../', f_name='test.parquet')
 
 def _encode_dates(X):
     X = X.copy()  # modify a copy of X
@@ -21,6 +42,12 @@ def _encode_dates(X):
 
     # Finally we can drop the original columns from the dataframe
     return X.drop(columns=["date"])
+
+def _encode_cat(X):
+    X = X.copy()  # modify a copy of X
+
+    # Finally we can drop the original columns from the dataframe
+    return X.drop(columns=["counter_id", "site_id", "counter_technical_id", "latitude", "longitude"])
 
 
 def _merge_external_data(X):
@@ -52,7 +79,7 @@ def get_estimator():
             ("cat", categorical_encoder, categorical_cols),
         ]
     )
-    regressor = Ridge()
+    regressor = XGBRegressor(n_estimators=1000, max_depth=11, eta=0.1, subsample=0.9, colsample_bytree=0.6)
 
     pipe = make_pipeline(
         FunctionTransformer(_merge_external_data, validate=False),
@@ -62,3 +89,33 @@ def get_estimator():
     )
 
     return pipe
+
+
+pipe = get_estimator()
+pipe.fit(X_train, y_train)
+
+print(
+    f"Train set, RMSE={mean_squared_error(y_train, pipe.predict(X_train), squared=False):.2f}"
+)
+print(
+    f"Test set, RMSE={mean_squared_error(y_test, pipe.predict(X_test), squared=False):.2f}"
+)
+
+cv = TimeSeriesSplit(n_splits=6)
+
+# When using a scorer in scikit-learn it always needs to be better when smaller, hence the minus sign.
+scores = cross_val_score(
+    pipe, X_train, y_train, cv=cv, scoring="neg_root_mean_squared_error"
+)
+print("RMSE: ", scores)
+print(f"RMSE (all folds): {-scores.mean():.3} ± {(-scores).std():.3}")
+
+y_pred = pipe.predict(X_final)
+
+results = pd.DataFrame(
+    dict(
+        Id=np.arange(y_pred.shape[0]),
+        log_bike_count=y_pred,
+    )
+)
+results.to_csv("submission.csv", index=False)
